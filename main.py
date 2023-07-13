317b9bfb02df52245887f7b43dec7534cf773191
JAAR <joe.aaron.acosta@outlook.com>
2023-07-12 23:21:00 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 07/12/2023
+>>> Practicing Fundamentals Program 5
+>>> Version 0
+'''
+
+'''
+>>> Results in a program that prints all numbers from 1 to 100, but for multiples of three, print "Fizz" instead, and for multiples of five, print "Buzz." For numbers that are multiples of both three and five, print "FizzBuzz."
+'''
+
+def main() :
+    print('Hello World!')
+
+if __name__ == '__main__' :
+    main()
